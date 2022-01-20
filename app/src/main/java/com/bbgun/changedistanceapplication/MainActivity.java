63b0b72b891552b7a5cda892f4b683890ae378ab
tomatopumpkin <6206021621147@fitm.kmutnb.ac.th>
2022-01-20 16:43:51 +0700

    update

--- app/src/main/java/com/bbgun/changedistanceapplication/MainActivity.java
+++ app/src/main/java/com/bbgun/changedistanceapplication/MainActivity.java
@@ -3,12 +3,37 @@ package com.bbgun.changedistanceapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText kiloText;
+    private Button btnCal;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        kiloText = (EditText) findViewById(R.id.kiloText);
+                //(EditText) => new EditText();
+        btnCal = (Button) findViewById(R.id.btnCal);
+
+        btnCal.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+               float kilo , mile;
+               kilo = Integer.parseInt(kiloText.getText().toString());
+               mile = kilo * 5 / 8;
+               Toast.makeText( MainActivity.this, mile + "miles. " , Toast.LENGTH_LONG).show();
+
+            }
+        });
+
+
     }
 }
